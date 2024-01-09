bbb5dbaf5026b106b69a1654a6de5b08deee9b4c
CheesyLiker <72305420+CheesyLiker@users.noreply.github.com>
2024-01-09 05:22:20 +0300

    Update bss_test.lua

--- bss_test.lua
+++ bss_test.lua
@@ -1,32 +1,54 @@
-pcall(function()
-    local function getEventInfo(Event, Name)
-        print("\n----------["..Name.." Information]----------\n")
-        for mainIndex, mainValue in pairs(Event) do
-            if type(mainValue) == "table" then
-               print("\n[Main Table -> SubTable] Name: "..mainIndex.."\n")
-               for subTableIndex1, subTableValue1 in pairs(mainValue) do
-                   if type(subTableValue1) == "table" then 
-                       print("\n[Main Table -> SubTable -> SubTable] Name: "..subTableIndex1.."\n")
-                       for subTableIndex2, subTableValue2 in pairs(subTableValue1) do
-                            print("Index: "..subTableIndex2, "Value: "..subTableValue2)
-                       end
-                   else
-                       print("Index: "..subTableIndex1, "Value: "..subTableValue1)
+
+local function getEventInfo(Event, Name)
+    print("\n----------["..Name.." Information]----------\n")
+    for mainIndex, mainValue in pairs(Event) do
+        if type(mainValue) == "table" then
+           print("\n[Main Table -> SubTable] Name: "..mainIndex.."\n")
+           for subTableIndex1, subTableValue1 in pairs(mainValue) do
+               if type(subTableValue1) == "table" then 
+                   print("\n[Main Table -> SubTable -> SubTable] Name: "..subTableIndex1.."\n")
+                   for subTableIndex2, subTableValue2 in pairs(subTableValue1) do
+                        print("Index: "..subTableIndex2, "Value: "..subTableValue2)
                    end
+               else
+                   print("Index: "..subTableIndex1, "Value: "..subTableValue1)
                end
-            else
-                print("Index: "..mainIndex, "Value: "..mainValue)
-            end
+           end
+        else
+            print("Index: "..mainIndex, "Value: "..mainValue)
         end
-        print("\n")
     end
-    
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStatsSummary"):InvokeServer(), "RetrievePlayerStatsSummary")
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStat"):InvokeServer(), "RetrievePlayerStat")
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("ItemPackageEvent"):InvokeServer(), "ItemPackageEvent")
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrieveBeeInfoSummary"):InvokeServer(), "RetrieveBeeInfoSummary")
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("WindShrineDonation"):InvokeServer(), "WindShrineDonation")
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("GetPromoInfo"):InvokeServer(), "GetPromoInfo")
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStats"):InvokeServer(), "RetrievePlayerStats")
-    getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrieveCachedPlayerStat"):InvokeServer(), "RetrieveCachedPlayerStat")
+    print("\n")
+end
+pcall(function()
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrieveCachedPlayerStat"):InvokeServer(), "RetrieveCachedPlayerStat")
+end)
+
+pcall(function()
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStats"):InvokeServer(), "RetrievePlayerStats")
+end)
+
+pcall(function()
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("GetPromoInfo"):InvokeServer(), "GetPromoInfo")
+end)
+
+pcall(function()
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("WindShrineDonation"):InvokeServer(), "WindShrineDonation")
+end)
+
+pcall(function()
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrieveBeeInfoSummary"):InvokeServer(), "RetrieveBeeInfoSummary")
+end)
+
+pcall(function()
+
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("ItemPackageEvent"):InvokeServer(), "ItemPackageEvent")
+end)
+
+pcall(function()
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStat"):InvokeServer(), "RetrievePlayerStat")
+end)
+
+pcall(function()
+getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStatsSummary"):InvokeServer(), "RetrievePlayerStatsSummary")
 end)
