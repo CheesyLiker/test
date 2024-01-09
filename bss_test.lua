d86206efcd87234c8440398dc781fe8750b34f7d
CheesyLiker <72305420+CheesyLiker@users.noreply.github.com>
2024-01-10 02:27:33 +0300

    Update bss_test.lua

--- bss_test.lua
+++ bss_test.lua
@@ -1,55 +1,30 @@
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/ParmesanHubStorage/Storage/main/ParmesanHub_UI_Lib.lua')))()
 
-local function getEventInfo(Event, Name)
-    print("\n----------["..Name.." Information]----------\n")
-    for mainIndex, mainValue in pairs(Event) do
-        if type(mainValue) == "table" then
-           print("\n[Main Table -> SubTable] Name: "..mainIndex.."\n")
-           for subTableIndex1, subTableValue1 in pairs(mainValue) do
-               if type(subTableValue1) == "table" then 
-                   print("\n[Main Table -> SubTable -> SubTable] Name: "..subTableIndex1.."\n")
-                   for subTableIndex2, subTableValue2 in pairs(subTableValue1) do
-                        print("Index: "..subTableIndex2, "Value: "..subTableValue2)
-                   end
-               else
-                   print("Index: "..subTableIndex1, "Value: "..subTableValue1)
-               end
-           end
-        else
-            print("Index: "..mainIndex, "Value: "..mainValue)
-        end
-    end
-    print("\n")
-end
---[[
-pcall(function()
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrieveCachedPlayerStat"):InvokeServer(), "RetrieveCachedPlayerStat")
-end)
+local Main = Library:Init({
+	name = "D-DAY PRIVATE SCRIPT"
+})
 
-pcall(function()
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStats"):InvokeServer(), "RetrievePlayerStats")
-end)
+local Tab = Main:CreateTab({
+	name = "Delete Structures",
+	icon = "rbxassetid://15101013637"
+})
 
-pcall(function()
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("GetPromoInfo"):InvokeServer(), "GetPromoInfo")
-end)
+local Button = Tab:Button({
+	name = "Delete Obstacles",
+	callback = (function()
+		pcall(function()
+			game.Workspace.Map.Vegetation:Destroy()
+			game.Workspace.Map.Boundaries:Destroy()
+			game.Workspace.Map.Buildings:Destroy()
+		end)
+	end)
+})
 
-pcall(function()
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("WindShrineDonation"):InvokeServer(), "WindShrineDonation")
-end)
-]]--
-pcall(function()
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrieveBeeInfoSummary"):InvokeServer(), "RetrieveBeeInfoSummary")
-end)
-
-pcall(function()
-
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("ItemPackageEvent"):InvokeServer(), "ItemPackageEvent")
-end)
-
-pcall(function()
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStat"):InvokeServer(), "RetrievePlayerStat")
-end)
-
-pcall(function()
-getEventInfo(game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("RetrievePlayerStatsSummary"):InvokeServer(), "RetrievePlayerStatsSummary")
-end)
+local Button = Tab:Button({
+	name = "Delete Terrain",
+	callback = (function()
+		pcall(function()
+			game.Terrain:Clear()
+		end)
+	end)
+})
