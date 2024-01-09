6a57d3c1592705e71153b96e32a2dc4d6b9395d3
CheesyLiker <72305420+CheesyLiker@users.noreply.github.com>
2024-01-10 02:50:24 +0300

    Update bss_test.lua

--- bss_test.lua
+++ bss_test.lua
@@ -1,5 +1,71 @@
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/ParmesanHubStorage/Storage/main/ParmesanHub_UI_Lib.lua')))()
 
+local TweenService = game:GetService("TweenService")
+local platform = nil
+
+local function MoveCharacter(tweenPart, waitTime, movePos, isDown)
+	local TweenPart = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+	local HRP = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+	local Info = TweenInfo.new(
+		waitTime,
+		Enum.EasingStyle.Linear,
+		Enum.EasingDirection.InOut,
+		0,
+		false,
+		0
+	)
+
+	if isDown then
+		HRP.CFrame = HRP.CFrame + Vector3.new(0, -50, 0)
+	end
+
+	platform.CFrame = HRP.CFrame + Vector3.new(0, -3, 0)
+
+	-- Character Move
+	local TweenGoals = {CFrame = movePos}
+	local Tween = TweenService:Create(TweenPart, Info, TweenGoals)
+	Tween:Play()
+
+	-- Platform Move
+	TweenGoals = {CFrame = movePos + Vector3.new(0, -3, 0)}
+	TweenPart = platform
+	Tween = TweenService:Create(TweenPart, Info, TweenGoals)
+	Tween:Play()
+	
+	task.wait(waitTime)
+end
+
+local function MoveToFlag(flag)
+	
+	local HRP = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") 
+	local Flags = game.Workspace.Map.Flags
+	
+	-- Turn Off Player Collision
+	for index, bodyPart in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+		if (bodyPart.ClassName == "Part" or bodyPart.ClassName == "MeshPart") and bodyPart.CanCollide == true then
+			bodyPart.CanCollide = false
+		end
+	end
+	
+	-- Create a Platform
+	platform = nil
+	if not game.workspace:FindFirstChild("AutofarmPlatform") then
+		platform = Instance.new("Part", game.workspace)
+		platform.Name = "AutofarmPlatform"
+		platform.Size = Vector3.new(15, 0.5, 15)
+		platform.Transparency = 1
+		platform.Anchored = true
+	end
+	platform = game.workspace.AutofarmPlatform
+	
+	MoveCharacter(HRP, 40, CFrame.new(flag.Position.X, HRP.Position.Y, flag.Position.Z), true)
+
+	MoveCharacter(HRP, 10, flag.CFrame + Vector3.new(0, -27, 0), false)
+end
+
+
 local Main = Library:Init({
 	name = "D-DAY PRIVATE SCRIPT"
 })
@@ -39,3 +105,48 @@ local Button = Tab:Button({
 		end)
 	end)
 })
+
+local Tab = Main:CreateTab({
+	name = "Flags",
+	icon = "rbxassetid://15101013637"
+})
+
+Tab:Section({
+	text = "Omaha Beach"
+})
+
+local Button = Tab:Button({
+	name = "Beach",
+	callback = (function()
+		MoveToFlag(game.Workspace.Map.Flags.Beach.Pole)
+	end)
+})
+
+local Button = Tab:Button({
+	name = "Village",
+	callback = (function()
+		MoveToFlag(game.Workspace.Map.Flags.Village.Pole)
+	end)
+})
+
+local Button = Tab:Button({
+	name = "Forest",
+	callback = (function()
+		MoveToFlag(game.Workspace.Map.Flags.Forest.Pole)
+	end)
+})
+
+local Button = Tab:Button({
+	name = "City",
+	callback = (function()
+		MoveToFlag(game.Workspace.Map.Flags.City.Pole)
+	end)
+})
+
+local Button = Tab:Button({
+	name = "Stronghold",
+	callback = (function()
+		MoveToFlag(game.Workspace.Map.Flags.Stronghold.Pole)
+	end)
+})
+
